49c7946a5c4eb06e5db1f51a86237cd2912df0d0
Milan <kmilanm13@gmail.com>
2023-09-29 22:28:31 +0100

    [Milan] A new beginning

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { MainContentComponent } from './components/main-content/main-content.component';
+import {NgOptimizedImage} from "@angular/common";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    MainContentComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgOptimizedImage,
   ],
   providers: [],
   bootstrap: [AppComponent]
