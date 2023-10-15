4d6fce16f0d6601b4a09a6e34d5a24124879f3db
Milan <kmilanm13@gmail.com>
2023-10-15 21:37:29 +0100

    Revert "[Milan] Blogs button redirects you to blogs page."
    This reverts commit f0696b2e727798f8af2dcf7c07966d5684a05e5a.

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,16 +1,22 @@
 import { Component } from '@angular/core';
-import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
+//   template: `
+//   <main>
+//     <a [routerLink]="['/']">
+//       <header class="brand-name">
+//         <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true">
+//       </header>
+//     </a>
+//     <section class="content">
+//       <router-outlet></router-outlet>
+//     </section>
+//   </main>
+// `,
 })
 export class HomeComponent {
 
-  constructor(private router: Router) {
-  }
-  goToBlogs(): void{
-    this.router.navigate(['blogs']);
-  }
 }
