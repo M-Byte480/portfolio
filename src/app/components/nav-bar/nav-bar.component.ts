997aba8c427dc99e2ec7e6b6fa2b626f4a96cf09
Milan <kmilanm13@gmail.com>
2024-01-30 15:25:02 +0000

    [Milan] Highlights button based on active route

--- src/app/components/nav-bar/nav-bar.component.ts
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -1,13 +1,24 @@
-import { Component } from '@angular/core';
-import {Router} from "@angular/router";
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute, Router} from "@angular/router";
 
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
   styleUrls: ['./nav-bar.component.css']
 })
-export class NavBarComponent {
+export class NavBarComponent implements OnInit{
+  currentRoute: string = this.router.url;
   constructor(private router: Router) {
+  }
+
+  ngOnInit(): void {
+    this.changeHighlight();
+  }
+
+  changeHighlight(): void{
+    setTimeout((): void => {
+      this.currentRoute = this.router.url.substring(1);
+    }, 100);
   }
 
 }
